7148a2e197c60ddaebc5bfc39020f2c92db44728
msh <alstngusqkqh@gmail.com>
2025-08-18 20:27:47 +0900

    배열 생성 및 배열의 사용법

---
+++ thisisjava/src/ch05/sec06/ArrayCreateByValueListExample.java
@@ -0,0 +1,30 @@
+package ch05.sec06;
+
+public class ArrayCreateByValueListExample {
+    public static void main(String[] args) {
+        // 배열 변수 선언과 배열 생성
+        String[] season = {"Spring", "Summer", "Autumn", "Winter"};
+
+        // 배열의 항목값 읽기
+        System.out.println("season[0]: " + season[0]);
+        System.out.println("season[1]: " + season[1]);
+        System.out.println("season[2]: " + season[2]);
+        System.out.println("season[3]: " + season[3]);
+
+        // 인덱스 1번 항목의 값 변경
+        season[1] = "여름";
+        System.out.println("season[1]: " + season[1]);
+        System.out.println();
+
+        // 배열 변수 선언과 배열 생성
+        int[] score = {83, 90, 87};
+        int sum = 0;
+        for (int i = 0; i < score.length; i++) {
+            sum += score[i];
+        }
+
+        System.out.println("총합: " + sum);
+        double average = (double) sum / score.length;
+        System.out.println("평균: " + average);
+    }
+}

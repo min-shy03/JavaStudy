2ee79233f7ccabd1c40aa71b548b2386ccbe29d6
msh <alstngusqkqh@gmail.com>
2025-08-21 18:18:32 +0900

    정적 멤버 static 사용

---
+++ thisisjava/src/ch06/sec10/exam01/Calculator.java
@@ -0,0 +1,13 @@
+package ch06.sec10.exam01;
+
+public class Calculator {
+    static double pi = 3.141592;
+
+    static int plus(int a, int b) {
+        return a + b;
+    }
+
+    static int minus(int a, int b) {
+        return a - b;
+    }
+}
